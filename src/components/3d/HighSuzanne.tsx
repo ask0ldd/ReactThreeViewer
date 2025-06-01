8093c62ebdb5b2c334fc1101b7637a542550ed18
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-06-01 18:31:12 +0200

    *edit* front / high res suzanne model / traversing children logic memo

--- src/components/3d/HighSuzanne.tsx
+++ src/components/3d/HighSuzanne.tsx
@@ -18,6 +18,7 @@ export function HighSuzanne({suzanneRotation} : {suzanneRotation : boolean}) {
   const { nodes } = useGLTF('highsuz.glb') as unknown as GLTFResult
 
   const meshRef = useRef<Mesh>(null)
+  // const newTexture = useTexture(textureUrl);
 
   // rotation around the Y axis
   useFrame((_, delta) => {
@@ -29,6 +30,19 @@ export function HighSuzanne({suzanneRotation} : {suzanneRotation : boolean}) {
   useEffect(() => {
     if(!suzanneRotation && meshRef.current) meshRef.current.rotation.y = 0
   }, [suzanneRotation])
+
+  /*
+  React.useEffect(() => {
+    // Traverse the scene to find the mesh you want to modify
+    scene.traverse((child) => {
+      if (child.isMesh && child.name === 'TargetMeshName') { // Replace with your mesh's name
+        child.material.map = newTexture;
+        child.material.needsUpdate = true;
+        meshRef.current = child;
+      }
+    });
+  }, [scene, newTexture]);
+  */
 
   return (
     <group dispose={null}>
