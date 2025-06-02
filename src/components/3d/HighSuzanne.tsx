8836cf5294a41cf5bb9d35936ff937d49b599303
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-06-02 06:06:47 +0200

    *edit* front / suzanne component : ao + texture flip

--- src/components/3d/HighSuzanne.tsx
+++ src/components/3d/HighSuzanne.tsx
@@ -2,6 +2,7 @@ import { useGLTF } from "@react-three/drei"
 import { useFrame } from "@react-three/fiber"
 import { useEffect, useRef } from "react"
 import { Mesh } from "three"
+import { useTexture } from "@react-three/drei"
 
 
 type GLTFResult = {
@@ -14,22 +15,25 @@ type GLTFResult = {
 }
 
 
-export function HighSuzanne({suzanneRotation} : {suzanneRotation : boolean}) {
-  const { nodes } = useGLTF('highsuz.glb') as unknown as GLTFResult
+export function HighSuzanne({rotation, ao} : {rotation : boolean, ao : boolean}) {
+  const { nodes } = useGLTF('highsuz2.glb') as unknown as GLTFResult
 
   const meshRef = useRef<Mesh>(null)
   // const newTexture = useTexture(textureUrl);
 
+  const aoMap = useTexture("ao.png")
+  aoMap.flipY = false
+
   // rotation around the Y axis
   useFrame((_, delta) => {
-    if (meshRef.current && suzanneRotation) {
+    if (meshRef.current && rotation) {
       meshRef.current.rotation.y += delta // time elapsed between frames
     }
   })
 
   useEffect(() => {
-    if(!suzanneRotation && meshRef.current) meshRef.current.rotation.y = 0
-  }, [suzanneRotation])
+    if(!rotation && meshRef.current) meshRef.current.rotation.y = 0
+  }, [rotation])
 
   /*
   React.useEffect(() => {
@@ -44,6 +48,15 @@ export function HighSuzanne({suzanneRotation} : {suzanneRotation : boolean}) {
   }, [scene, newTexture]);
   */
 
+  useEffect(() => {
+    if (nodes.Suzanne.geometry && !nodes.Suzanne.geometry.attributes.uv2) {
+      nodes.Suzanne.geometry.setAttribute(
+        'uv2',
+        nodes.Suzanne.geometry.attributes.uv
+      )
+    }
+  }, [nodes.Suzanne.geometry])
+
   return (
     <group dispose={null}>
       <mesh
@@ -52,12 +65,15 @@ export function HighSuzanne({suzanneRotation} : {suzanneRotation : boolean}) {
         receiveShadow
         geometry={nodes.Suzanne.geometry}
         material={nodes.Suzanne.material}
-        position={[0, -0.5, 0]}
+        position={[0, 0.5, 0]}
+        scale={1.5}
       >
         <meshStandardMaterial
           metalness={1}
           roughness={0.3}
           color="silver"
+          aoMap={aoMap}
+          aoMapIntensity={+ao / 1.5}
         />
       </mesh>
     </group>
