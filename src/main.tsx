78e043865e0ab3e7b78be819c86e6280d6ef8ca4
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-06-03 03:05:06 +0200

    *edit* front / routing implementation

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Scenery from './Scenery.tsx'
+import Route from './router/Route.tsx'
+import { RouterProvider } from './router/RouterProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider base={"http://localhost:5173/ReactThreeViewer"}>
+      <Route path={""} element={<App />} />
+      <Route path={"/"} element={<App />} />
+      <Route path={"/scene"} element={<Scenery />} />
+    </RouterProvider>
   </StrictMode>,
 )
