f4093275c8a388a56ed61c3db122c3bab51e8593
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-06-05 09:36:24 +0200

    *edit* front / new routing paths

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import App from './App.tsx'
 import Scenery from './Scenery.tsx'
 import Route from './router/Route.tsx'
 import { RouterProvider } from './router/RouterProvider.tsx'
+import Scenery2 from './Scenery2.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider base={"http://localhost:5173/ReactThreeViewer"}>
       <Route path={""} element={<App />} />
       <Route path={"/"} element={<App />} />
+      <Route path={"/scene2"} element={<Scenery2 />} />
       <Route path={"/scene"} element={<Scenery />} />
     </RouterProvider>
   </StrictMode>,
